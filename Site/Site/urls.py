66216e9348c04faea56c342d0c268e09658d0d86
amelia <>
2021-11-25 12:18:41 -0800

    Make site redirect from root to /posts/

--- Site/Site/urls.py
+++ Site/Site/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/posts/')),
     path('posts/', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
